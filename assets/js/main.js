54973dc9a8b2ab3fb62030e2e90fb86aa2724b02
Paulina Rojas <lindaliel@gmail.com>
2017-06-23 12:19:43 -0400

    lat y long mapa: Stgo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ var map;  /*inicializo el mapa*/
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
-    center: new google.maps.LatLng(-33.91722, 151.23064),
+    center: new google.maps.LatLng(-33.45, -70.66667),
     mapTypeId: 'roadmap'
   });
 
